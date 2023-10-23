a0931ffac8a5d5f15e0baad083443fbcb512d476
RIDER <cpaneln@gmail.com>
2023-10-23 11:02:59 +0530

    DONE

---
+++ src/main/java/com/gaurang/restfullapiusingspring/RestfullApiUsingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.gaurang.restfullapiusingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullApiUsingSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfullApiUsingSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gaurang/restfullapiusingspring/RestfullApiUsingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurang.restfullapiusingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullApiUsingSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

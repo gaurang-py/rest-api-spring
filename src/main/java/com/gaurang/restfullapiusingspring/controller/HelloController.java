fec4807617add5eeba6a9977238d7ae8fc80182e
RIDER <cpaneln@gmail.com>
2023-10-23 11:12:22 +0530

    DONE

--- src/main/java/com/gaurang/restfullapiusingspring/controller/HelloController.java
+++ src/main/java/com/gaurang/restfullapiusingspring/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.gaurang.restfullapiusingspring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +11,19 @@ public class HelloController {
     public String helloMessage(){
         return "Hello";
     }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password) {
+
+        if (isValidUser(username, password)) {
+            return "Login successful!";
+        } else {
+            return "Login failed!";
+        }
+    }
+    private boolean isValidUser(String username, String password) {
+        return "admin".equals(username) && "admin".equals(password);
+    }
 }
+
+
